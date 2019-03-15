6f8eea2135454d3208a9e3fbe50b5c0c703fcc75
Avdon Racki <avdon@racki.ca>
2019-03-14 21:29:48 -0400

    change framework to 4.7.2

--- Data Structures (Linked List)/Data Structures (Linked List)/Form1.Designer.cs
+++ Data Structures (Linked List)/Data Structures (Linked List)/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1204, 755);
+            this.ClientSize = new System.Drawing.Size(1208, 842);
             this.Name = "Form1";
             this.Text = "Big-O Linked List";
             this.ResumeLayout(false);

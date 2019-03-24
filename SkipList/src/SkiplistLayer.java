a168a444b7183c9229913aa9d3f3f76f88cb8ddf
Avdon Racki <avdon@racki.ca>
2019-03-24 14:43:24 -0400

    created constructor

--- SkipList/src/SkiplistLayer.java
+++ SkipList/src/SkiplistLayer.java
@@ -9,13 +9,35 @@ public class SkiplistLayer {
 		layer = -1;
 		
 		//header
-		list.add(new SkiplistCell("inf"));
-		//tail
 		list.add(new SkiplistCell("-inf"));
+		//tail
+		list.add(new SkiplistCell("inf"));
 	}
 	
 	public SkiplistLayer(int layer, boolean isSorted, int[] arr) {
-		
+		this.layer = layer;
+		list.add(new SkiplistCell("-inf"));
+		for (int i = 0; i < arr.length; i++){
+			if (list.get(i).linkLeft == Integer.MIN_VALUE){
+				list.add(new SkiplistCell(i,Integer.MIN_VALUE,arr[i+1]));
+			}
+			else if (list.get(i).linkRight == Integer.MAX_VALUE){
+				list.add(new SkiplistCell(i,arr[i-1],Integer.MAX_VALUE));
+			}
+			else{
+				list.add(new SkiplistCell(i,arr[i-1],arr[i+1]));
+			}
+		}
+		list.add(new SkiplistCell("inf"));
 	}
-	
+
+	public void setLayer() {
+
+	}
+
+	public void getLayer() {
+
+	}
+
+
 }

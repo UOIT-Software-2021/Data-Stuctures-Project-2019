dd1b3a847791432f6923785a7cfd55a4809555b2
djnucci <djnucci@hotmail.com>
2019-03-25 14:40:19 -0400

    removed depricated code

--- SkipList/src/SkipList.java
+++ SkipList/src/SkipList.java
@@ -3,14 +3,15 @@ import java.util.Arrays;
 
 public class SkipList {
 	private ArrayList<SkiplistLayer> list;
+	private int height, width;
 
 	/**
 	 * new skiplist with only a header and a tail
 	 */
 	public SkipList() {
+		height = 0;
 		list = new ArrayList<SkiplistLayer>();
-
-		
+		width = list.size();
 	}
 
 	/**
@@ -20,38 +21,6 @@ public class SkipList {
 	public SkipList(int[] arr) {
 		list = new ArrayList<SkiplistLayer>();
 		
-		//header
-		//list.add(new SkiplistCell("inf"));
-		
-		//check if the input data is sorted
-		boolean isInputSorted = false;
-		for (int i = 0; i < arr.length - 1; i++) {
-			if (arr[i] > arr [i + 1]) {
-				isInputSorted = false;
-				break;
-			}
-			if (arr[i] <= arr [i + 1]) {
-				isInputSorted = true;
-			}
-		}
-		
-		//if not sorted, sort it
-		if (!isInputSorted) {
-			Arrays.sort(arr);
-		}
-		
-		//add all elements to the list
-		for (int i = 0; i < arr.length; i++) {
-			if (i + 1 < arr.length) {
-				//list.add(new SkiplistCell(arr[i], 0, 0, list.get(list.size() - 1).getValue(), list.get(list.size() + 1).getValue()));
-			}
-			else {
-				//list.add(new SkiplistCell(arr[i], 0, 0, list.get(list.size() - 1).getValue(), Integer.MAX_VALUE));
-			}
-		}
-		
-		//tail
-		//list.add(new SkiplistCell("-inf"));
 		
 	}
 	
@@ -59,10 +28,11 @@ public class SkipList {
 	public String toString() {
 		String str = "";
 		
-		for (int i = 0; i < list.size(); i++) {
-			for (int j = 0; j < list.size(); j++) {
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
 				
 			}
+			str += "\n";
 		}
 		
 		return null;
